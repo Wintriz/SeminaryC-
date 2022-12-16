46f3b566fb4bd8c5073aa0e84e3d1ed6379e243d
Andrey Kuzmin <120460173+Wintriz@users.noreply.github.com>
2022-12-16 10:36:35 +0300

    Add files via upload

---
+++ Zadanie_2/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 2 Даны два числа. Показать большее и меньшее число
+Console.WriteLine("Enter a number a: ");
+Console.WriteLine("Enter a number b: ");
+int a = int.Parse(Console.ReadLine()!);
+int b = int.Parse(Console.ReadLine()!);
+if(a > b)
+{
+    max = a;
+    min = b;
+    Console.WriteLine("max: ");
+Console.WriteLine(a);
+    Console.WriteLine("min: ");
+Console.WriteLine(b);    
+}
+
+else
+{
+    max = b;
+    min = a;
+    Console.WriteLine("max: ");
+Console.WriteLine(b);
+    Console.WriteLine("min: ");
+Console.WriteLine(a);
+}
+
+
