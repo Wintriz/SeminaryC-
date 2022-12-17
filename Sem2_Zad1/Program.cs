96ef52c082f35fd2e404a848938a3d55379f76ab
Andrey <rf5800@mail.ru>
2022-12-17 11:56:03 +0300

    Sem2_Zad1

---
+++ Sem2_Zad1/Program.cs
@@ -0,0 +1,18 @@
+﻿// //Задача 1. Напишите программу, которая будет принимать на вход два 
+// числа и выводить, является ли второе число кратным первому. 
+// Если второе число не кратно числу первому, то программа выводит 
+// остаток от деления.
+Console.WriteLine("Enter first number: ");
+int FirstNumber = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Enter second number: ");
+int SecondNumber = int.Parse(Console.ReadLine()!);
+
+if (FirstNumber % SecondNumber == 0)
+{
+    Console.Write("Первое число кратно второму");
+}
+else
+{
+Console.Write("Вывод остатка: ");
+Console.WriteLine(FirstNumber/SecondNumber);
+}
