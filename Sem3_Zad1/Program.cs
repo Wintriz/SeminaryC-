1ab5d07ab51e8ad2aeff9dda26d79d0ae101e8f5
Andrey <rf5800@mail.ru>
2022-12-19 22:16:19 +0300

    Sem3

---
+++ Sem3_Zad1/Program.cs
@@ -0,0 +1,59 @@
+﻿// Напишите программу, которая принимает на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка и показывает диапазон 
+//возможных координат точек в этой четверти (x и y).
+// A (2, 3) -> 1 четверть, x > 0, y > 0
+// A (-5, -8) -> 3 четверть, x < 0, y < 0
+// Console.WriteLine("Введите координату Х");
+// int x = int.Parse(Console.ReadLine()!);
+// Console.WriteLine("Введите координату у");
+// int y = int.Parse(Console.ReadLine()!);
+// if (x > 0 && y > 0)
+// {
+//     Console.WriteLine("1");
+// }
+// if (x < 0 && y < 0)
+// {
+//     Console.WriteLine("3");
+// }
+// if (x < 0 && y > 0)
+// {
+//     Console.WriteLine("4");
+// }
+// if (x > 0 && y < 0)
+// {
+//     Console.WriteLine("2");
+// }
+
+
+
+// Задача 3. Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+// 5 -> 1, 4, 9, 16, 25.
+// 2 -> 1,4
+
+
+// Console.Write("Введите число N ");
+// int N = int.Parse(Console.ReadLine());
+// int i = 1;
+// //if (N<0) N=N*(-1);
+// while (i<=N)
+// {
+// Console.WriteLine(" " + i*i);
+// i++;
+// }
+
+
+// Задача 2.
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+// A (3,6); B (2,1) -> 5,09
+// A (7,-5); B (1,-1) -> 7,21
+// Console.WriteLine("Введите первое число: ");
+// double a = double.Parse(Console.ReadLine());
+// Console.WriteLine("Введите второе число: ");
+// double b = double.Parse(Console.ReadLine());
+// Console.WriteLine("Введите третье число: ");
+// double a1 = double.Parse(Console.ReadLine());
+// Console.WriteLine("Введите четвертое число: ");
+// double b1 = double.Parse(Console.ReadLine());
+
+// double ab = Math.Sqrt(Math.Pow(a-a1,2) + Math.Pow(b - b1, 2));
+
+// Console.WriteLine(ab);
